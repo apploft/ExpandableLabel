cc2e73e77b55030a2667f2c263bad226b90d34d6
Mathias Koehnke <mathias.koehnke@web.de>
2015-06-29 14:21:50 +0200

    Initial Commit

---
+++ ExpandableLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandableLabelDemo
+//
+//  Created by Mathias Köhnke on 29/06/15.
+//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
